0e6e7cdeb4ce6f399f552c990b0c4b4cca61fc92
nagitausu <nagitaosu.radio@gmail.com>
2019-07-10 16:46:13 +0900

    Add script for csv parse

---
+++ csvparser.js
@@ -0,0 +1,23 @@
+function getCSV(){
+    var req = new XMLHttpRequest();
+    req.open("get", "data.csv", true);
+    req.send(null);
+	
+    req.onload = function(){
+	convertCSVtoArray(req.responseText);
+    }
+}
+ 
+function convertCSVtoArray(str){
+    var result = [];
+    var tmp = str.split("\n");
+ 
+    for(var i=0;i<tmp.length;++i){
+        result[i] = tmp[i].split(',');
+    }
+	result.map(function(element){return Number(element);});
+ 
+    alert(result[1][2]);
+}
+ 
+getCSV();
